8708264200555e6034b5415c2a2b32896741a8db
pjbuonomo <81430048+pjbuonomo@users.noreply.github.com>
2024-02-06 15:33:32 -0500

    Create Startup.cs

---
+++ Startup.cs
@@ -0,0 +1,22 @@
+public void ConfigureServices(IServiceCollection services)
+{
+    services.AddScoped<IDataService, LocalDataService>();
+    services.AddDbContextFactory<ApplicationDbContext>(options =>
+        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+    services.AddQuickTableEntityFrameworkAdapter();
+    services.AddRazorPages();
+    services.AddServerSideBlazor();
+    
+    services.AddScoped<ICodeSnippetService, LocalSnippetService>();
+    services.AddDocs();
+}
+
+
+
+
+  "ConnectionStrings": {
+    "DefaultConnection": "Server=YourServer;Database=YourDatabase;User=YourUser;Password=YourPassword;"
+  },
+
+
+
