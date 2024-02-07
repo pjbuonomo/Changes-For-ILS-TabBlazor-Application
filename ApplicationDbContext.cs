bd2d470d0f8c03e77db744e327dc6807a611b8d1
pjbuonomo <81430048+pjbuonomo@users.noreply.github.com>
2024-02-06 20:00:41 -0500

    Create ApplicationDbContext.cs

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using Tabler.Docs.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Country> Countries { get; set; } = default!;
+    public DbSet<MarketQuote> MarketQuotes { get; set; } = default!; // Add this line
+
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Country>().HasKey(x => x.Code);
+        modelBuilder.Entity<Country>().OwnsOne(x => x.Medals);
+
+        // If you need to configure anything specific for MarketQuote, you can do it here.
+    }
+}
