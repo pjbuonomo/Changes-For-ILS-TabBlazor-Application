b1656bbf7736b7448c3aa488543aadc0322c19a9
pjbuonomo <81430048+pjbuonomo@users.noreply.github.com>
2024-02-07 13:48:52 -0500

    Update QuickTablesEF.cs

--- QuickTablesEF.cs
+++ QuickTablesEF.cs
@@ -28,3 +28,26 @@
 Severity	Code	Description	Project	File	Line	Suppression State	Details
 Error	CS0411	The type arguments for method 'TypeInference.CreateQuickTable_0_CaptureParameters<TGridItem>(IQueryable<TGridItem>, out IQueryable<TGridItem>, PaginationState, out PaginationState, string, out string)' cannot be inferred from the usage. Try specifying the type arguments explicitly.	Tabler.Docs	\\ad-its.credit-agricole.fr\Amundi_Boston\Homedirs\buonomo\@Config\Desktop\ILS Application\ILS-Application-V1\docs\Tabler.Docs\Microsoft.NET.Sdk.Razor.SourceGenerators\Microsoft.NET.Sdk.Razor.SourceGenerators.RazorSourceGenerator\Components_QuickTables_QuickTablesEF_razor.g.cs	90	Active	
 Error	CS0103	The name 'DbContextFactory' does not exist in the current context	Tabler.Docs	\\ad-its.credit-agricole.fr\Amundi_Boston\Homedirs\buonomo\@Config\Desktop\ILS Application\ILS-Application-V1\docs\Tabler.Docs\Components\QuickTables\QuickTablesEF.razor	46	Active	
+
+@using Microsoft.EntityFrameworkCore
+@using Tabler.Docs.Data // Adjust this to your actual namespace
+@inject IServiceProvider ServiceProvider
+
+<div class="card">
+    <QuickTable Items="marketQuotes" Pagination="@pagination" Class="quick-table-example">
+        <!-- Your PropertyColumn definitions here -->
+    </QuickTable>
+    <Paginator Value="@pagination"/>
+</div>
+
+@code {
+    private PaginationState pagination = new PaginationState { ItemsPerPage = 15 };
+    private List<MarketQuote>? marketQuotes;
+
+    protected override async Task OnInitializedAsync()
+    {
+        using var scope = ServiceProvider.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        marketQuotes = await dbContext.MarketQuotes.ToListAsync();
+    }
+}
